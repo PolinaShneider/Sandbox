c5e94cb0d4c2d559792811c1ac0a6e6ccb424d9e
PolinaShneider <beatrixlinkoln@gmail.com>
2019-10-05 11:00:40 +0300

    Tasks 05.09

--- contest.js
+++ contest.js
@@ -2539,3 +2539,13 @@ console.assert(JSON.stringify(
 ) === JSON.stringify(
     [["eat", "tea", "ate"], ["tan", "nat"], ["bat"]]
 ), "groupAnagrams #1");
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function (num) {
+    return num ? num - 9 * Math.floor((num - 1) / 9) : 0;
+};
+
+console.assert(addDigits(11) === 2, "addDigits #1");
