98b0c58e66d7095f1190826025b2251cda12d67d
Polina Shneider <beatrixlinkoln@gmail.com>
2019-10-10 20:34:54 +0300

    Update contest.js

--- contest.js
+++ contest.js
@@ -2642,3 +2642,42 @@ console.assert(
 console.assert(
     findContentChildren([1, 2], [1, 2, 3]) === 2, "findContentChildren #2"
 );
+
+/*
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+/**
+ * Explanation: https://medium.com/confessions-of-a-bootcamp-grad/how-to-solve-leetcodes-convert-sorted-array-to-binary-search-tree-problem-with-javascript-a61e6d6d6c36
+ */
+var sortedArrayToBST = function (nums) {
+    //base cases
+    if (nums.length === 1) return new TreeNode(nums[0]);
+    if (nums.length === 0) return null;
+
+    //create a new TreeNode(center)
+    let centerIdx = Math.floor(nums.length / 2);
+    let root = new TreeNode(nums[centerIdx]);
+
+    //set left node to center of left subtree
+    let leftSubtree = nums.slice(0, centerIdx);
+    root.left = sortedArrayToBST(leftSubtree);
+
+    //set right node to center of right subtree
+    let rightSubtree = nums.slice(centerIdx + 1, nums.length);
+    root.right = sortedArrayToBST(rightSubtree);
+
+    return root;
+};
