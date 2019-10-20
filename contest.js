cf8b7cbac071c3aa15e6631448439d1c9f2cf8e6
Polina Shneider <beatrixlinkoln@gmail.com>
2019-10-20 08:25:06 +0300

    Tasks 20.10

--- contest.js
+++ contest.js
@@ -2968,3 +2968,16 @@ const tree = {
 };
 
 console.assert(minDiffInBST(tree) === 1, "minDiffInBST #1");
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+    }
+    return Math.max(...nums)
+};
+
+console.assert(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]) === 6, "maxSubArray #1");
