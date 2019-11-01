6bf09220572448f225ce3a5d42496f65cc1a6937
PolinaShneider <beatrixlinkoln@gmail.com>
2019-11-01 10:00:04 +0300

    Tasks 01.11

--- contest.js
+++ contest.js
@@ -3227,3 +3227,30 @@ console.assert(backspaceCompare("ab#c", "ad#c") === true, "backspaceCompare #1")
 console.assert(backspaceCompare("ab##", "ab##") === true, "backspaceCompare #2");
 console.assert(backspaceCompare("a##c", "#a#c") === true, "backspaceCompare #3");
 console.assert(backspaceCompare("a#c", "b") === false, "backspaceCompare #4");
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+    if (head === null) {
+        return head;
+    }
+
+    let current = head;
+    while (current != null && current.next != null) {
+        if (current.val === current.next.val) {
+            current.next = current.next.next
+        } else {
+            current = current.next
+        }
+    }
+    return head
+};
