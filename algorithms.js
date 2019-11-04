9e8d5654e0080f8def828cad9b1aa3dbff25151e
Polina Shneider <beatrixlinkoln@gmail.com>
2019-11-04 20:15:06 +0300

    BFS, BST

--- algorithms.js
+++ algorithms.js
@@ -19,35 +19,34 @@ function binary(elem) {
         }
     }
 
-    return undefined;
+    return null;
 }
 
 const root = {
-    "children": [
+    children: [
         {
-            "children": [
+            children: [
                 {
-                    "children": [],
-                    "val": 5
+                    children: [],
+                    val: 5
                 },
                 {
-                    "$id": "6",
-                    "children": [],
-                    "val": 6
+                    children: [],
+                    val: 6
                 }
             ],
-            "val": 3
+            val: 3
         },
         {
-            "children": [],
-            "val": 2
+            children: [],
+            val: 2
         },
         {
-            "children": [],
-            "val": 4
+            children: [],
+            val: 4
         }
     ],
-    "val": 1
+    val: 1
 };
 
 function dfs(root) {
@@ -60,10 +59,9 @@ function dfs(root) {
     return values;
 }
 
-function dfsStack(root) {
-    const stack = [];
+function bfs(root) {
+    const stack = [root];
     const values = [];
-    stack.push(root);
     while (stack.length) {
         let current = stack.pop();
         values.push(current.val);
@@ -76,5 +74,117 @@ function dfsStack(root) {
     return values;
 }
 
-console.log(dfs(root));
-console.log(dfsStack(root));
+console.log(`        1
+      / | \\
+     2  3  4
+       / \\
+      5  6`);
+console.log("DFS", dfs(root));
+console.log("BFS", bfs(root));
+console.log("\n");
+
+/**
+ * Traversal explanation: https://js-algorithms.tutorialhorizon.com/2015/10/12/binary-tree-traversal/
+ *
+ *          30
+ *        /    \
+ *      36     21
+ *       \     / \
+ *       35   26 15
+ *        \       \
+ *        33      8
+ */
+const binaryTree = {
+    val: 30,
+    left: {
+        val: 36,
+        right: {
+            val: 35,
+            left: null,
+            right: {
+                val: 33,
+                left: null,
+                right: null
+            }
+        },
+        left: null
+    },
+    right: {
+        val: 21,
+        left: {
+            val: 26,
+            right: null,
+            left: null
+        },
+        right: {
+            val: 15,
+            right: {
+                val: 8,
+                right: null,
+                left: null
+            },
+            left: null
+        }
+    }
+};
+
+console.log(`           30
+         /    \\
+       36     21
+        \\     / \\
+        35   26 15
+         \\       \\
+         33      8`);
+
+function binaryPreOrderTraversal(root) {
+    // pre-order
+    let values = [root.val];
+
+    if (root.left) {
+        values = values.concat(binaryPreOrderTraversal(root.left));
+    }
+    if (root.right) {
+        values = values.concat(binaryPreOrderTraversal(root.right));
+    }
+
+    return values;
+}
+
+console.log("pre-order: root -> left -> right", binaryPreOrderTraversal(binaryTree));
+
+function binaryPostOrderTraversal(root) {
+    // post-order
+    let values = [];
+
+    if (root.left) {
+        values = values.concat(binaryPostOrderTraversal(root.left));
+    }
+    if (root.right) {
+        values = values.concat(binaryPostOrderTraversal(root.right));
+    }
+
+    values.push(root.val);
+
+    return values;
+}
+
+console.log("post-order: left -> right -> root", binaryPostOrderTraversal(binaryTree));
+
+function binaryInOrderTraversal(root) {
+    // in-order
+    let values = [];
+
+    if (root.left) {
+        values = values.concat(binaryInOrderTraversal(root.left));
+    }
+
+    values.push(root.val);
+
+    if (root.right) {
+        values = values.concat(binaryInOrderTraversal(root.right));
+    }
+
+    return values;
+}
+
+console.log("in-order: left -> root -> right", binaryInOrderTraversal(binaryTree));
