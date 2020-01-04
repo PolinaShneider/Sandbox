1a98827276eac08c522e5fa0a0f04e083dc0bad8
Polina Shneider <beatrixlinkoln@gmail.com>
2020-01-04 23:46:32 +0300

    Tasks 04.01

--- sorting.js
+++ sorting.js
@@ -11,3 +11,87 @@ function insertionSort(arr) {
 
     return arr;
 }
+
+const list = {
+    val: 4,
+    next: {
+        val: 2, next: {
+            val: 1, next: {
+                val: 3,
+                next: null
+            }
+        }
+    }
+}
+
+var insertionSortList = function (head) {
+    if (!head) {
+        return null;
+    }
+    let sorted = head;
+    head = head.next;
+    sorted.next = null;
+    while (head) {
+        let prev = null;
+        let node = sorted;
+        while (node && head.val > node.val) {
+            prev = node;
+            node = node.next;
+        }
+        let insert = head;
+        head = head.next;
+        insert.next = node;
+        if (prev) {
+            prev.next = insert;
+        } else {
+            sorted = insert;
+        }
+    }
+    return sorted;
+};
+
+/**
+* @param {ListNode} head
+* @return {ListNode}
+*/
+var insertionSortList = function (headref) {
+    // Initialize sorted linked list 
+    let sorted = null;
+    let current = headref;
+
+    // Traverse the given  
+    // linked list and insert every 
+    // node to sorted 
+    while (current != null) {
+        // Store next for next iteration 
+        let next = current.next;
+
+        // insert current in sorted linked list 
+        sortedInsert(current);
+
+        // Update current 
+        current = next;
+    }
+
+    function sortedInsert(newnode) {
+        /* Special case for the head end */
+        if (sorted == null || sorted.val >= newnode.val) {
+            newnode.next = sorted;
+            sorted = newnode;
+        }
+        else {
+            current = sorted;
+
+            /* Locate the node before the point of insertion */
+            while (current.next != null &&
+                current.next.val < newnode.val) {
+                current = current.next;
+            }
+            newnode.next = current.next;
+            current.next = newnode;
+        }
+    }
+
+    // Update head_ref to point to sorted linked list 
+    return sorted;
+};
