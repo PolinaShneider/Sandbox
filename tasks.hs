1e5d7c6fca1e3ed030ff11844cc3240b1d940ead
Polina Shneider <beatrixlinkoln@gmail.com>
2020-01-16 23:33:32 +0300

    FP practice

--- tasks.hs
+++ tasks.hs
@@ -1,2 +1,55 @@
+import Data.Char
+
 lenVec3 x y z = sqrt (x ^ 2 + y ^ 2 + z ^ 2)
-main = print $ lenVec3 2 3 6
+print $ lenVec3 2 3 6
+
+infixl 6 |-|
+(|-|) a b = abs (a - b)
+print $ 5 |-| 7
+
+twoDigits2Int :: Char -> Char -> Int
+twoDigits2Int x y = if isDigit x && isDigit y then digitToInt x * 10 +  digitToInt y else 100
+print $ twoDigits2Int '4' '2'
+
+dist :: (Double, Double) -> (Double, Double) -> Double
+dist p1 p2 = sqrt $ (fst p2 - fst p1) ^ 2 + (snd p2 - snd p1) ^ 2
+print $ dist (0,0) (2,2)
+
+doubleFact :: Integer -> Integer
+doubleFact n = if n <= 0 then 1 else n * doubleFact (n - 2)
+print $ doubleFact 7
+
+fibonacci n
+  | n == -1 = 1
+  | n == -2 = -1
+  | n == 0 = 0
+  | n == 1 = 1
+  | n < 0 = fibonacci (n + 2) - fibonacci (n + 1)
+  | n > 0 = fibonacci (n - 1) + fibonacci (n - 2)
+  | otherwise = error "something went wrong"
+
+print $ fibonacci (-10)
+
+fibonacci = helper 0 1
+  where
+    helper curr prev n
+      | n == 0 = curr
+      | n > 0 = helper (curr + prev) curr (n - 1)
+      | n < 0 = helper prev (curr - prev) (n + 1)
+
+print $ fibonacci (-10)
+
+seqA :: Integer -> Integer
+seqA 0 = 1
+seqA 1 = 2
+seqA 2 = 3
+seqA n = s' 3 3 2 1 n
+
+s' :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer
+s' i a b c n
+  | (i < n) = s' (i + 1) res a b n
+  | (i == n) = res
+  where
+    res = a + b - 2 * c
+
+print $ seqA 2
