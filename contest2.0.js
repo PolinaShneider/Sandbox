b70949fd790522daff2718eeb7faef662f853613
Polina Shneider <beatrixlinkoln@gmail.com>
2020-02-08 16:44:50 +0300

    Tasks 08.02

--- contest2.0.js
+++ contest2.0.js
@@ -1553,3 +1553,14 @@ var checkPerfectNumber = function (num) {
         }, 0)
     }
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    for (let i = 0; i < k; i++) {
+        nums.unshift(nums.pop());
+    }
+};
