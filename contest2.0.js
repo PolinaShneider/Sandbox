0464a3564ef38464c1b03d2c55172af07a32fd0e
Polina Shneider <beatrixlinkoln@gmail.com>
2020-02-12 21:25:36 +0300

    Tasks 12.02

--- contest2.0.js
+++ contest2.0.js
@@ -1580,3 +1580,23 @@ var sumZero = function(n) {
     
     return result;
 };
+
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps  = function(num) {
+    let steps = 0;
+    while (num) {
+        if (num % 2 == 0) {
+            num /= 2
+        } else {
+          num -= 1
+        }
+        
+        steps++;
+    }
+    
+    return steps;
+};
