7bbf726095b48d0e07f303368d2fb1cdc61c851d
Polina Shneider <beatrixlinkoln@gmail.com>
2020-02-18 22:57:01 +0300

    Tasks 18.02

--- contest2.0.js
+++ contest2.0.js
@@ -1643,3 +1643,21 @@ var moveZeroesToEnd = function (arr) {
 
     return arr;
 };
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countNegatives = function(grid) {
+    let counter = 0;
+    
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] < 0) {
+                counter++
+            }
+        }
+    }
+    
+    return counter;
+};
