bfec5b216162180432b3df6b18c930467be4be9e
Polina Shneider <beatrixlinkoln@gmail.com>
2020-04-23 17:05:17 +0300

    Tasks 23.04

--- contest3.0.js
+++ contest3.0.js
@@ -713,3 +713,19 @@ var subarraySum = function (nums, k) {
 
     return count;
 };
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var rangeBitwiseAnd = function(m, n) {
+  let trailingZero = 0;
+  while(m != n) {
+    m >>= 1;
+    n >>= 1;
+    trailingZero++;
+  }
+  
+  return m << trailingZero;
+};
