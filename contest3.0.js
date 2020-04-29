9f8dfd11495628b993deb06a2768940a37485961
Polina Shneider <beatrixlinkoln@gmail.com>
2020-04-29 20:49:45 +0300

    Tasks 29.04

--- contest3.0.js
+++ contest3.0.js
@@ -895,3 +895,40 @@ FirstUnique.prototype.add = function (value) {
  * var param_1 = obj.showFirstUnique()
  * obj.add(value)
  */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function (root) {
+    // keep max negative values to deal with negatives
+    let max = -Infinity;
+
+    function dfs(node) {
+        if (!node) {
+            return -Infinity
+        }
+
+        //call recursiverly dfs on left and right.
+        const leftSum = dfs(node.left);
+        const rightSum = dfs(node.right);
+        //find max of all the possible cases at that particular nodes.
+        const current = Math.max(node.val, (node.val + leftSum), (node.val + rightSum), (node.val + rightSum + leftSum))
+        //update max
+        max = Math.max(current, max);
+
+        // return path that parent should take to maximize sum.
+        return node.val + Math.max(leftSum, rightSum, 0)
+    }
+
+    dfs(root);
+    return max
+};
