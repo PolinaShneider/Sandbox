d87bc43faec117e9cceb9737d5cf633805db2260
Polina Shneider <beatrixlinkoln@gmail.com>
2020-05-15 18:26:22 +0300

    Tasks 15.05

--- contest3.0.js
+++ contest3.0.js
@@ -1254,3 +1254,39 @@ Trie.prototype.searchNode = function (word) {
  * var param_2 = obj.search(word)
  * var param_3 = obj.startsWith(prefix)
  */
+
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+var maxSubarraySumCircular = function (a) {
+    const aCopy = [...a];
+    const n = a.length;
+
+    let max_sum = kadane(a); // sliding window using kadane's algo
+    let max_wrap = 0;
+
+    for (let i = 0; i < n; i++) {
+        max_wrap += a[i]; // Calculate array-sum
+        a[i] = -a[i];  // invert the array (change sign)
+    }
+
+    max_wrap = max_wrap + kadane(a);
+    return (max_wrap > max_sum) ? max_wrap : max_sum === 0 ? max_sum = Math.max.apply(this, aCopy) : max_sum;
+
+    // Sliding window
+    function kadane(a) {
+        const n = a.length;
+        let max_so_far = 0, max_ending_here = 0;
+        for (let i = 0; i < n; i++) {
+            max_ending_here = max_ending_here + a[i];
+            if (max_ending_here < 0) {
+                max_ending_here = 0;
+            }
+            if (max_so_far < max_ending_here) {
+                max_so_far = max_ending_here;
+            }
+        }
+        return max_so_far;
+    }
+};
