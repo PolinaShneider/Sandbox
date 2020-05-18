d01882e3fba308ac2ad5de137af823987dd591bb
Polina Shneider <beatrixlinkoln@gmail.com>
2020-05-18 16:55:56 +0300

    Tasks 18.05

--- contest3.0.js
+++ contest3.0.js
@@ -1359,3 +1359,35 @@ var findAnagrams = function (s, p) {
 
     return res;
 };
+
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+    let start = 0;
+    let charsLeft = s1.length;
+    
+    //Create dictionary from s1
+    let dic = {};
+    for (let i = 0; i < s1.length; i++) {
+        dic[s1[i]] = (dic[s1[i]] || 0) + 1;
+    }
+    
+    
+    //Iterate s2
+    for (let i = 0; i < s2.length; i++) {
+        
+        //Decrease char in dictionary and check for completion
+        if (--dic[s2[i]] >= 0 && --charsLeft === 0) return true;
+        
+        //check if window is broken
+        while (start <= i && (isNaN(dic[s2[i]]) || dic[s2[i]] < 0)) {
+            if (++dic[s2[start++]] > 0) charsLeft++;
+        }
+        
+    }
+    
+    return false;
+};
