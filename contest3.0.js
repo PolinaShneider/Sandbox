94b75edc3ad7b180fefe8002104bc7f4db3d6380
Polina Shneider <beatrixlinkoln@gmail.com>
2020-05-20 19:15:01 +0300

    20.05

--- contest3.0.js
+++ contest3.0.js
@@ -1414,3 +1414,21 @@ StockSpanner.prototype.next = function(price) {
  * var obj = new StockSpanner()
  * var param_1 = obj.next(price)
  */
+
+/**
+ * @param {number[]} startTime
+ * @param {number[]} endTime
+ * @param {number} queryTime
+ * @return {number}
+ */
+var busyStudent = function(startTime, endTime, queryTime) {
+    let cnt = 0;
+    
+    for (let i = 0; i < startTime.length; i++) {
+        if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+            cnt++;
+        }
+    }
+    
+    return cnt;
+};
