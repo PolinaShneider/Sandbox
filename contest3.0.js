fb8c72759ccf0b2552aec170f87b89fda27f8ee7
Polina Shneider <beatrixlinkoln@gmail.com>
2020-05-24 19:11:48 +0300

    Tasks 24.05

--- contest3.0.js
+++ contest3.0.js
@@ -1484,3 +1484,19 @@ var intervalIntersection = function (A, B) {
 
     return res;
 };
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var sortString = function (s) {
+    s = s.split``.sort().join``;
+    let result = '';
+    while (s.length) {
+        result += s.match(/([a-z])(?!\1)/gi).join``;
+        s = s.replace(/([a-z])(?!\1)/gi, '');
+        result += (s.match(/([a-z])(?!\1)/gi) || []).reverse().join``;
+        s = s.replace(/([a-z])(?!\1)/gi, '');
+    }
+    return result
+};
