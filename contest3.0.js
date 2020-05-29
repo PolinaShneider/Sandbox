3ef96dde419d40c6d456dd54865068035a3d1770
Polina Shneider <beatrixlinkoln@gmail.com>
2020-05-29 17:04:21 +0300

    Tasks 29.05

--- contest3.0.js
+++ contest3.0.js
@@ -1554,6 +1554,44 @@ var possibleBipartition = function (N, dislikes) {
 };
 
 /**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function (numCourses, prerequisites) {
+    if (!prerequisites.length || !prerequisites[0].length) return true;
+    let indegrees = new Array(numCourses).fill(0); // 0 -> n-1
+    let graph = new Map(); //HashMap<Integer, int[]>
+    let queue = [];
+
+    for (let i = 0; i < prerequisites.length; i++) {
+        indegrees[prerequisites[i][0]]++;
+        let key = prerequisites[i][1];
+        if (graph.has(key)) {
+            graph.set(key, graph.get(key).concat(prerequisites[i][0]));
+        } else {
+            graph.set(key, [prerequisites[i][0]]);
+        }
+    }
+    indegrees.forEach((indegree, index) => {
+        if (indegree === 0) queue.push(index);
+    });
+    while (queue.length) {
+        let cur = queue.shift();
+        let courses = graph.get(cur);
+        for (let i = 0; courses && i < courses.length; i++) {
+            if (--indegrees[courses[i]] === 0) queue.push(courses[i]);
+        }
+    }
+    for (let i = 0; i < indegrees.length; i++) {
+        if (indegrees[i] !== 0) {
+            return false;
+        }
+    }
+    return true;
+};
+
+/**
  * Definition for singly-linked list.
  * function ListNode(val) {
  *     this.val = val;
@@ -1580,6 +1618,6 @@ var mergeTwoLists = function (l1, l2) {
  * @param {number} num
  * @return {number[]}
  */
-var countBits = function(num) {
-    return Array(num+1).fill().map((_, i) => i == 0 ? 0 : i.toString(2).match(/1/g).length);
+var countBits = function (num) {
+    return Array(num + 1).fill().map((_, i) => i == 0 ? 0 : i.toString(2).match(/1/g).length);
 };
