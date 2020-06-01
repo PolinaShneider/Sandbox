f07edfb89c1c659545d7ab50f5469e97ae0c71e2
Polina Shneider <beatrixlinkoln@gmail.com>
2020-06-01 22:22:42 +0300

    Tasks 01.06

--- contest3.0.js
+++ contest3.0.js
@@ -1667,3 +1667,31 @@ var minDistance = function (word1, word2) {
     }
     return M[0][0];
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+    function invert(node) {
+        if (!node) return;
+
+        const temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+
+        invert(node.left);
+        invert(node.right);
+    }
+
+    invert(root);
+    return root;
+};
