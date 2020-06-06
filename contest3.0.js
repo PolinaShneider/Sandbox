531fb1bf228dcdf6d97174f38acf160c7b5950cd
Polina Shneider <beatrixlinkoln@gmail.com>
2020-06-06 21:15:10 +0300

    Tasks 06.06

--- contest3.0.js
+++ contest3.0.js
@@ -1752,3 +1752,21 @@ Solution.prototype.pickIndex = function () {
  * var obj = new Solution(w)
  * var param_1 = obj.pickIndex()
  */
+
+/**
+ * @param {number[][]} people
+ * @return {number[][]}
+ */
+var reconstructQueue = function (people) {
+    const compare = ([h1, k1], [h2, k2]) => {
+        if (h1 !== h2) return h2 - h1;
+        else return k1 - k2;
+    };
+    people.sort(compare);
+
+    const res = [];
+    for (const p of people) {
+        res.splice(p[1], 0, p); // insert person at index k
+    }
+    return res;
+};
