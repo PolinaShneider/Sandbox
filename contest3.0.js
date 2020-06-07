0480a17407b348774dd7d3df89686e6feb121082
Polina Shneider <beatrixlinkoln@gmail.com>
2020-06-07 22:02:18 +0300

    Tasks 07.06

--- contest3.0.js
+++ contest3.0.js
@@ -1770,3 +1770,16 @@ var reconstructQueue = function (people) {
     }
     return res;
 };
+
+var change = function (amount, coins) {
+    const dp = [];
+    dp[0] = 1;
+
+    for (let i = 0; i < coins.length; ++i) {
+        for (let j = coins[i]; j <= amount; ++j) {
+            dp[j] = (dp[j] || 0) + (dp[j - coins[i]] || 0);
+        }
+    }
+
+    return isNaN(dp[amount]) ? 0 : dp[amount];
+};
