8e8c6e6e61a790bf54f500140e88ea126a779884
Polina Shneider <beatrixlinkoln@gmail.com>
2020-06-18 20:20:56 +0300

    Tasks 18.06

--- contest3.0.js
+++ contest3.0.js
@@ -1959,3 +1959,23 @@ var solve = function (board) {
         }
     }
 };
+
+var hIndex = function (citations) {
+
+    let start = 0, end = citations.length - 1;
+
+    while (start <= end) {
+
+        let mid = Math.floor((start + end) / 2);
+
+        if (citations.length - mid - 1 < citations[mid]) {
+
+            end = mid - 1;
+        } else {
+
+            start = mid + 1;
+        }
+    }
+
+    return citations.length - start;
+};
