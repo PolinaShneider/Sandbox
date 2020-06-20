25ad6277feb964195fa04ba20f7ad24f4864b326
Polina Shneider <beatrixlinkoln@gmail.com>
2020-06-20 15:25:10 +0300

    Tasks 20.06

--- contest3.0.js
+++ contest3.0.js
@@ -2033,3 +2033,25 @@ function pow(b, exp, mod) {
     }
     return output;
 }
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+var getPermutation = function (n, k) {
+    let factorial = [1];
+    for (let i = 1; i <= n; i++) {
+        factorial[i] = i * factorial[i - 1];
+    }
+
+    const nums = Array.from({length: n}, (v, i) => i + 1);
+    let res = "";
+    for (let i = n; i > 0; i--) {
+        const index = Math.ceil(k / factorial[i - 1]); // decide to use which permutation set
+        res += nums[index - 1];
+        nums.splice(index - 1, 1);
+        k -= (factorial[i - 1] * (index - 1));
+    }
+    return res;
+};
