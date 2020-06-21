7885f590c2f984c916766304a051e32e66a0285a
Polina Shneider <beatrixlinkoln@gmail.com>
2020-06-21 21:46:31 +0300

    Tasks 21.06

--- contest3.0.js
+++ contest3.0.js
@@ -2055,3 +2055,28 @@ var getPermutation = function (n, k) {
     }
     return res;
 };
+
+/**
+ * @param {number[][]} dungeon
+ * @return {number}
+ */
+var calculateMinimumHP = function (dungeon) {
+    let nrows = dungeon.length;
+    let ncols = dungeon[0].length;
+
+    let dp = [];
+    for (let r = 0; r < nrows + 1; r++) {
+        dp[r] = [];
+        for (let c = 0; c < ncols + 1; c++) {
+            dp[r][c] = Number.MAX_SAFE_INTEGER;
+        }
+    }
+    dp[nrows - 1][ncols] = dp[nrows][ncols - 1] = 1;
+
+    for (let r = nrows - 1; r >= 0; r--) {
+        for (let c = ncols - 1; c >= 0; c--) {
+            dp[r][c] = Math.max(1, Math.min(dp[r + 1][c], dp[r][c + 1]) - dungeon[r][c]);
+        }
+    }
+    return dp[0][0];
+};
