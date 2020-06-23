82e7ce4ee8793c4df6bdde6609cfdd54b3822cb3
Polina Shneider <beatrixlinkoln@gmail.com>
2020-06-23 23:13:06 +0300

    Tasks 23.06

--- contest3.0.js
+++ contest3.0.js
@@ -2088,3 +2088,22 @@ var calculateMinimumHP = function (dungeon) {
 var divisorGame = function (N) {
     return (N % 2 === 0);
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function (root) {
+    if (!root) {
+        return 0;
+    }
+    return countNodes(root.left) + countNodes(root.right) + 1;
+};
