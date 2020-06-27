aed05f202fd889378bb13cc76233195419a10819
Polina Shneider <beatrixlinkoln@gmail.com>
2020-06-27 13:42:51 +0300

    Tasks 27.06

--- contest3.0.js
+++ contest3.0.js
@@ -2119,3 +2119,39 @@ var numTrees = function (n) {
 function factorial(num) {
     return (num <= 0) ? 1 : num * factorial(num - 1);
 }
+
+var sumNumbers = function (root) {
+    function traverse(node, num) {
+        if (!node) {
+            return null;
+        }
+
+        num += node.val;
+
+        if (!node.left && !node.right) {
+            return +num;
+        }
+
+        return traverse(node.left, num) + traverse(node.right, num);
+    }
+
+    return traverse(root, '');
+};
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numSquares = function (n) {
+    const dp = new Array(n + 1).fill(Infinity);
+    dp[0] = 0;
+
+    for (let i = 1; i <= Math.floor(Math.sqrt(n)); i++) {
+        let squaredVal = i * i;
+        for (let j = squaredVal; j < dp.length; j++) {
+            dp[j] = Math.min(dp[j], 1 + dp[j - squaredVal])
+        }
+    }
+
+    return dp[n]
+};
