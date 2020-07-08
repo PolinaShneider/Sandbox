f28e310d528f454f1854115275b737d7a5a4ed08
Polina Shneider <beatrixlinkoln@gmail.com>
2020-07-08 19:46:42 +0300

    Tasks 08.07

--- contest3.0.js
+++ contest3.0.js
@@ -2392,3 +2392,12 @@ function dfs(node, leaves) {
     // it's a left leave if it doesn't have child nodes
     if (node.left && !node.left.left && !node.left.right) leaves.push(node.left.val);
 }
+
+/**
+ * @param {string} date1
+ * @param {string} date2
+ * @return {number}
+ */
+var daysBetweenDates = function (date1, date2) {
+    return Math.abs(new Date(date2) - new Date(date1)) / (24 * 60 * 60 * 1000);
+};
