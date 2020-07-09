06086b55e06b45247fb2ba57265aec6b03012abb
Polina Shneider <beatrixlinkoln@gmail.com>
2020-07-09 22:08:23 +0300

    Tasks 09.07

--- contest3.0.js
+++ contest3.0.js
@@ -2401,3 +2401,34 @@ function dfs(node, leaves) {
 var daysBetweenDates = function (date1, date2) {
     return Math.abs(new Date(date2) - new Date(date1)) / (24 * 60 * 60 * 1000);
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var widthOfBinaryTree = function (root) {
+    const minPos = [0];
+    let maxWidth = 0;
+
+    callDFS(root, 0, 0);
+    return maxWidth;
+
+    function callDFS(node, level, pos) {
+        if (!node) return;
+        if (minPos[level] === undefined) minPos.push(pos);
+
+        const diff = pos - minPos[level];
+        maxWidth = Math.max(maxWidth, diff + 1);
+
+        callDFS(node.left, level + 1, diff * 2);
+        callDFS(node.right, level + 1, diff * 2 + 1);
+    }
+};
