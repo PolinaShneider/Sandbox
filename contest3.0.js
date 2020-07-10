02c8c03c336799702ba4602dc8f8716373e3a864
Polina Shneider <beatrixlinkoln@gmail.com>
2020-07-10 22:22:58 +0300

    Tasks 10.07

--- contest3.0.js
+++ contest3.0.js
@@ -2432,3 +2432,42 @@ var widthOfBinaryTree = function (root) {
         callDFS(node.right, level + 1, diff * 2 + 1);
     }
 };
+
+/**
+ * // Definition for a Node.
+ * function Node(val,prev,next,child) {
+ *    this.val = val;
+ *    this.prev = prev;
+ *    this.next = next;
+ *    this.child = child;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var flatten = function (head) {
+    if (!head) return head;
+
+    function traverse(node) {
+        if (!node.next && !node.child) return node;
+
+        if (node.child) {
+            const nextNode = node.next;
+            node.next = node.child;
+            node.next.prev = node;
+            node.child = null;
+
+            if (nextNode) {
+                const tailNode = traverse(node.next);
+                tailNode.next = nextNode;
+                nextNode.prev = tailNode;
+            }
+        }
+        return traverse(node.next);
+    }
+
+    traverse(head);
+    return head;
+};
