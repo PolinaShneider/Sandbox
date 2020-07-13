f560eed0747b86e7b86684f40b291da64915364a
Polina Shneider <beatrixlinkoln@gmail.com>
2020-07-13 22:43:20 +0300

    Tasks 13.07

--- contest3.0.js
+++ contest3.0.js
@@ -2488,3 +2488,30 @@ var lengthOfLastWord = function (s) {
 var reverseBits = function (n) {
     return Number.parseInt(n.toString(2).split("").reverse().join("").padEnd(32, "0"), 2);
 };
+
+/**
+ * @param {number[]} houses
+ * @param {number[]} heaters
+ * @return {number}
+ */
+var findRadius = function (houses, heaters) {
+    heaters.sort((a, b) => a - b);
+    return Math.max(...houses.map(h => findMinDistance(h, heaters)));
+};
+
+const findMinDistance = (house, heaters) => {
+    let left = 0;
+    let right = heaters.length - 1;
+    while (left <= right) {
+        const mid = left + ((right - left) >> 1);
+        if (heaters[mid] <= house && house <= heaters[mid + 1]) {
+            return Math.min(house - heaters[mid], heaters[mid + 1] - house);
+        } else if (heaters[mid] <= house) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    if (left === 0) return heaters[0] - house;
+    if (left === heaters.length) return house - heaters[heaters.length - 1];
+};
