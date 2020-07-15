8602eccf237cc8e7852b02564a2de9406150e9db
Polina Shneider <beatrixlinkoln@gmail.com>
2020-07-16 00:28:33 +0300

    Tasks 16.07

--- contest3.0.js
+++ contest3.0.js
@@ -2528,3 +2528,36 @@ var angleClock = function (hour, minutes) {
 
     return diff > 180 ? 360 - diff : diff;
 };
+
+/**
+ * @param {number[]} A
+ * @return {boolean}
+ */
+var validMountainArray = function (A) {
+    if (A.length <= 2) {
+        return false;
+    }
+    let direction = 1; // 1 for up and 0 for down;
+    let previous = A[0];
+
+    for (let i = 1; i < A.length; ++i) {
+        let current = A[i];
+
+        if (current > previous) {
+            if (direction !== 1) {
+                return false;
+            }
+            previous = current;
+        } else if (current < previous) {
+            if (i === 1) {
+                return false;
+            }
+            direction = 0;
+            previous = current;
+        } else {
+            return false;
+        }
+    }
+
+    return direction === 0;
+};
