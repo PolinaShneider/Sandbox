015c9019085ce2adba1924f44d03282db8fc7df4
Polina Shneider <beatrixlinkoln@gmail.com>
2020-07-28 20:02:48 +0300

    Tasks 28.07

--- contest3.0.js
+++ contest3.0.js
@@ -2838,3 +2838,31 @@ var buildTree = function (inorder, postorder) {
 
     return callDFS(inorder);
 };
+
+/**
+ * @param {character[]} tasks
+ * @param {number} n
+ * @return {number}
+ */
+var leastInterval = function (tasks, n) {
+    const tasksMap = Array(26).fill(0);
+    for (const task of tasks) {
+        tasksMap[task.charCodeAt(0) - 65] += 1;
+    }
+
+    // sort in reverse order
+    tasksMap.sort((a, b) => b - a);
+
+    const maxVal = tasksMap[0] - 1;
+    let idleSlots = maxVal * n;
+
+    for (let i = 1; i < 26; i++) {
+        if (tasksMap[i] === 0) {
+            break;
+        }
+
+        idleSlots -= Math.min(tasksMap[i], maxVal);
+    }
+
+    return idleSlots > 0 ? idleSlots + tasks.length : tasks.length;
+};
