9aa088c65e859e4b7defec722c81747e52fa25da
Polina Shneider <beatrixlinkoln@gmail.com>
2020-08-11 23:06:30 +0300

    Tasks 11.08

--- contest4.0.js
+++ contest4.0.js
@@ -303,3 +303,16 @@ var isAnagram = function (s, t) {
 
     return s.length === t.length && map.size === 0;
 };
+
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function (citations) {
+    citations.sort((a, b) => a - b);
+    let i = 0;
+    while (i < citations.length && citations[citations.length - 1 - i] > i) {
+        i++;
+    }
+    return i;
+};
