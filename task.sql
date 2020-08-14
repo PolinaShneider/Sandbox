c47e680c42361bf33f902cffc9d62b842899562f
Polina Shneider <beatrixlinkoln@gmail.com>
2020-08-15 00:10:12 +0300

    Update task.sql

--- task.sql
+++ task.sql
@@ -24,3 +24,19 @@ from cinema a join cinema b
   on abs(a.seat_id - b.seat_id) = 1
   and a.free = true and b.free = true
 order by a.seat_id;
+
+# Write your MySQL query statement below
+SELECT name FROM `customer` WHERE referee_id IS NULL OR referee_id <> 2;
+
+# Write your MySQL query statement below
+DELETE t1 FROM `Person` t1
+INNER JOIN `Person` t2
+WHERE
+    t1.Id > t2.Id AND
+    t1.Email = t2.Email;
+
+# Write your MySQL query statement below
+DELETE t1 FROM Person t1, Person t2
+WHERE
+    t1.Id > t2.Id AND
+    t1.Email = t2.Email;
