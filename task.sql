c3646161a1a809acb0d8f1d8ba65e0954c64069c
Polina Shneider <beatrixlinkoln@gmail.com>
2020-08-14 23:17:12 +0300

    Tasks 14.08

--- task.sql
+++ task.sql
@@ -18,3 +18,9 @@ SELECT MAX(num)as num FROM (
     SELECT num, COUNT(num) as cnt FROM `my_numbers` GROUP BY num
 ) as T WHERE cnt = 1;
 
+# Write your MySQL query statement below
+select distinct a.seat_id
+from cinema a join cinema b
+  on abs(a.seat_id - b.seat_id) = 1
+  and a.free = true and b.free = true
+order by a.seat_id;
