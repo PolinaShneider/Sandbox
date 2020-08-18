918a7c6a9817ce2747caffcaf08f4304e4f003d5
Polina Shneider <beatrixlinkoln@gmail.com>
2020-08-18 23:48:52 +0300

    Tasks 18.08

--- contest4.0.js
+++ contest4.0.js
@@ -484,3 +484,22 @@ var removePalindromeSub = function (s) {
 
     return s === s.split('').reverse().join('') ? 1 : 2;
 };
+
+var numsSameConsecDiff = function (N, K) {
+    const res = [];
+    var compose = function (n) {
+        if (n.length === N) {
+            res.push(n)
+        } else {
+            let num = Number(n[n.length - 1]);
+            if (num - K >= 0) compose(`${n}${num - K}`);
+            if (num + K <= 9 && K > 0) compose(`${n}${num + K}`);
+        }
+
+    };
+    for (let i = 0; i < 10; i++) {
+        if (N > 1 && i === 0) continue;
+        compose(`${i}`)
+    }
+    return res
+};
