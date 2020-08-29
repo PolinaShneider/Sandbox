5b6dffe4ddabc7f04d73c81c1cde84c938c48212
Polina Shneider <beatrixlinkoln@gmail.com>
2020-08-29 18:52:55 +0300

    Tasks 29.08

--- contest3.0.js
+++ contest3.0.js
@@ -3553,3 +3553,27 @@ var pancakeSort = function (A) {
 
     return results;
 };
+
+/**
+ * @param {number[]} salary
+ * @return {number}
+ */
+var average = function (salary) {
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+
+    for (let i = 0; i < salary.length; i++) {
+        if (salary[i] > max) {
+            max = salary[i];
+        }
+
+        if (salary[i] < min) {
+            min = salary[i];
+        }
+
+        sum += salary[i];
+    }
+
+    return (sum - min - max) / (salary.length - 2)
+};
