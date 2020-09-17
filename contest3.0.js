2fd3fc033e0a879c01c0472ac71e29dc8b1d1711
Polina Shneider <beatrixlinkoln@gmail.com>
2020-09-17 21:10:27 +0300

    Tasks 16.09

--- contest3.0.js
+++ contest3.0.js
@@ -3759,3 +3759,19 @@ var findMaximumXOR = function (nums) {
 
     return max;
 };
+
+/**
+ * @param {string} instructions
+ * @return {boolean}
+ */
+var isRobotBounded = function (instructions) {
+    let dir = 0, mov = [0, 0, 0, 0];
+    for (let i = 0; i < instructions.length; i++) {
+        if (instructions[i] === "G") {
+            mov[dir]++;
+        } else {
+            dir = (dir + (instructions[i] === "L" ? 1 : 3)) % 4;
+        }
+    }
+    return !!(dir || (!(mov[0] - mov[2]) && !(mov[1] - mov[3])))
+};
