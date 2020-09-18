6437e894c5c22d8a52e94fedcc745553c80877d4
Polina Shneider <beatrixlinkoln@gmail.com>
2020-09-18 20:50:03 +0300

    Tasks 18.09

--- contest3.0.js
+++ contest3.0.js
@@ -3804,3 +3804,24 @@ var simplifyPath = function (path) {
 
     return '/' + result.reverse().join('/');
 };
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        }
+
+        if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice
+        }
+    }
+
+    return maxProfit;
+};
