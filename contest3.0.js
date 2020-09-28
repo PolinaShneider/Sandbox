c40b885d1e1392dccd928c238ea2530593085a98
Polina Shneider <beatrixlinkoln@gmail.com>
2020-09-29 02:17:41 +0300

    Tasks 29.09

--- contest3.0.js
+++ contest3.0.js
@@ -4095,3 +4095,21 @@ var updateMatrix = function (matrix) {
 
     return result;
 };
+
+var numSubarrayProductLessThanK = function (nums, k) {
+    let res = 0, count = 0, prod = 1;
+    let left = 0, right = 0;
+
+    while (right < nums.length) {
+        prod *= nums[right];
+        count++;
+        while (count && prod >= k) {
+            prod /= nums[left];
+            count--;
+            left++;
+        }
+        if (prod < k) res += count;
+        right++;
+    }
+    return res;
+};
