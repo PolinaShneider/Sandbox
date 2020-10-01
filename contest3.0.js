c6b89ab674fcac6d937478fffc22871417db6b99
Polina Shneider <beatrixlinkoln@gmail.com>
2020-10-01 23:06:43 +0300

    Tasks 01.10

--- contest3.0.js
+++ contest3.0.js
@@ -4142,3 +4142,27 @@ var firstMissingPositive = function (nums) {
 
     return max > 0 ? max + 1 : 1;
 };
+
+
+var RecentCounter = function () {
+    this.queue = [];
+};
+
+/**
+ * @param {number} t
+ * @return {number}
+ */
+RecentCounter.prototype.ping = function (t) {
+    this.queue.push(t);
+    // keep pings within 3000 from the latest ping
+    while (this.queue[0] < t - 3000) {
+        this.queue.shift();
+    }
+    return this.queue.length;
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
