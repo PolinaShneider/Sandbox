b62962a0a8b2ec148ccf533994d75cfc50acb8ce
Polina Shneider <beatrixlinkoln@gmail.com>
2020-10-06 23:12:13 +0300

    Tasks 06.10

--- trees.js
+++ trees.js
@@ -774,3 +774,41 @@ var maxDepth = function (root) {
 
     return levelOrder(root).length;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function (root, val) {
+    if (root === null) {
+        root = new TreeNode(val);
+        return root;
+    }
+
+    if (val > root.val) {
+        if (root.right == null) {
+            root.right = new TreeNode(val);
+        } else {
+            insertIntoBST(root.right, val)
+        }
+    }
+
+    if (val < root.val) {
+        if (root.left == null) {
+            root.left = new TreeNode(val);
+        } else {
+            insertIntoBST(root.left, val)
+        }
+    }
+
+    return root;
+};
