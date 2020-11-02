3887d76f3fc8d3437705c899947dc87c03ddeaac
Polina Shneider <beatrixlinkoln@gmail.com>
2020-11-02 21:25:27 +0300

    Cleanup

--- contest4.0.js
+++ contest4.0.js
@@ -1662,13 +1662,6 @@ var recoverTree = function (root) {
 };
 
 /**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
  * @param {ListNode} head
  * @return {number}
  */
