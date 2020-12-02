2f5be361afdd9f9efbf10c26a5ae7f5c7a1b27b0
Polina Shneider <beatrixlinkoln@gmail.com>
2020-12-03 00:24:19 +0300

    Tasks 2.12

--- contest4.0.js
+++ contest4.0.js
@@ -2350,3 +2350,42 @@ var shortestDistance = function (words, word1, word2) {
 
     return min;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param head The linked list's head.
+ Note that the head is guaranteed to be not null, so it contains at least one node.
+ * @param {ListNode} head
+ */
+var Solution = function (head) {
+    this.head = head;
+};
+
+/**
+ * Returns a random node's value.
+ * @return {number}
+ */
+Solution.prototype.getRandom = function () {
+    let i = 0, val;
+    let node = this.head;
+
+    while (node) {
+        i++;
+        if (Math.floor(Math.random() * i) + 1 === i) val = node.val;
+        node = node.next;
+    }
+
+    return val;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(head)
+ * var param_1 = obj.getRandom()
+ */
