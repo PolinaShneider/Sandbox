dd7610e570b14459fe02dd2b28b3d56f51f9c112
Polina Shneider <beatrixlinkoln@gmail.com>
2020-12-08 02:20:04 +0300

    Tasks 08.12

--- contest4.0.js
+++ contest4.0.js
@@ -2472,3 +2472,24 @@ var connect = function (root) {
 
     return root;
 };
+
+/**
+ * @param {number} n
+ * @return {number[][]}
+ */
+var generateMatrix = function (n) {
+    let mat = Array(n).fill(0).map(i => Array(n).fill(0))
+    let dir = [[0, 1], [-1, 0], [0, -1], [1, 0]]   //The directions in order, right, down, left, up
+    let i = 0, j = -1, num = 1
+
+    for (let x = 0; num <= n * n; x++) {
+        x = x % 4
+        i += dir[x][0], j += dir[x][1]
+        while (i >= 0 && j >= 0 && i < n && j < n && mat[i][j] === 0) {
+            mat[i][j] = num++
+            i += dir[x][0], j += dir[x][1]
+        }
+        i -= dir[x][0], j -= dir[x][1]
+    }
+    return mat
+};
