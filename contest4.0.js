c91b1e31169306deb6dd533fdf4dd2201ee4e6d4
Polina Shneider <beatrixlinkoln@gmail.com>
2020-12-21 01:41:03 +0300

    Tasks 20.12

--- contest4.0.js
+++ contest4.0.js
@@ -2681,3 +2681,36 @@ var increasingTriplet = function (nums) {
     }
     return false;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var plusOne = function (head) {
+    let first = new ListNode(0)
+    first.next = head
+    let farRightNine = first
+
+    while (head) {
+        if (head.val !== 9) {
+            farRightNine = head
+        }
+        head = head.next
+    }
+
+    farRightNine.val++
+    farRightNine = farRightNine.next
+
+    while (farRightNine) {
+        farRightNine.val = 0
+        farRightNine = farRightNine.next
+    }
+    return first.val === 0 ? first.next : first
+};
