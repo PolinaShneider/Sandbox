4057c223200498a2761e382639e937c79aeddc49
Polina Shneider <beatrixlinkoln@gmail.com>
2020-12-24 01:26:44 +0300

    Tasks 22.12

--- contest4.0.js
+++ contest4.0.js
@@ -2777,3 +2777,32 @@ var findNearestRightNode = function (root, u) {
     // u is for sure in the tree, then it wont reach here
     return null;
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nextGreaterElement = function (n) {
+    let arr = n.toString().split("");
+    let pivotIdx = -1;
+    for (let i = arr.length - 1; i > 0; i--) {
+        if (arr[i - 1] < arr[i]) {
+            pivotIdx = i - 1;
+            break;
+        }
+    }
+    if (pivotIdx == -1) return -1; // there is no next greater number
+    for (let i = arr.length - 1; i > pivotIdx; i--) {
+        if (arr[pivotIdx] < arr[i]) {
+            [arr[pivotIdx], arr[i]] = [arr[i], arr[pivotIdx]];
+            break;
+        }
+    }
+    let lSubarr = arr.slice(0, pivotIdx + 1);
+    let rSubarr = arr.slice(pivotIdx + 1);
+    rSubarr.reverse();
+    let res = Number(lSubarr.join("") + rSubarr.join(""));
+    return res <= 2 ** 31 - 1 ? res : -1;
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+};
