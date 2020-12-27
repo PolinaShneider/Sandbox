6db61340ea989d1034f4e84b40df5cecc20b53d7
Polina Shneider <beatrixlinkoln@gmail.com>
2020-12-27 03:21:36 +0300

    Tasks 26.12

--- contest4.0.js
+++ contest4.0.js
@@ -2852,3 +2852,16 @@ var findDiagonalOrder = function (matrix) {
     }
     return results.flat()
 };
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function (s, i = 0, memo = {}) {
+    if (i in memo) return memo[i];
+    if (s[i] === '0') return 0;
+    if (i >= s.length - 1) return 1;
+    memo[i] = numDecodings(s, i + 1, memo)
+        + (s[i] + s[i + 1] < 27 ? numDecodings(s, i + 2, memo) : 0);
+    return memo[i];
+};
