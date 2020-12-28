861c48342b7b914bca829142ea52950f41b88ae9
Polina Shneider <beatrixlinkoln@gmail.com>
2020-12-29 02:40:12 +0300

    Tasks 2.12

--- contest4.0.js
+++ contest4.0.js
@@ -2892,3 +2892,21 @@ var minJumps = function (A) {
     }
     return result
 };
+
+/**
+ * @param {number} target
+ * @return {number}
+ */
+var reachNumber = function (target) {
+    target = Math.abs(target);
+    let steps = 0, sum = 0;
+    while (sum < target) {
+        steps++;
+        sum += steps;
+    }
+    while ((sum - target) % 2 !== 0) {
+        steps++;
+        sum += steps;
+    }
+    return steps;
+};
