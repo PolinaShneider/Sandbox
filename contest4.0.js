71858c1a5a4ad9924b3a070c9e450dfddec7d6db
Polina Shneider <beatrixlinkoln@gmail.com>
2021-01-08 02:37:55 +0300

    Tasks 07.01

--- contest4.0.js
+++ contest4.0.js
@@ -3045,3 +3045,12 @@ var findKthPositive = function (arr, k) {
     }
     return i - 1;
 };
+
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+    return (BigInt(num1) * BigInt(num2)).toString();
+};
