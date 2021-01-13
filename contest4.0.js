0d29e71156dacbcd61a99367c752c4641bbeb4e2
Polina Shneider <beatrixlinkoln@gmail.com>
2021-01-14 02:13:33 +0300

    Tasks 13.01

--- contest4.0.js
+++ contest4.0.js
@@ -3065,6 +3065,26 @@ var arrayStringsAreEqual = function (word1, word2) {
 };
 
 /**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function (people, limit) {
+    let weights = new Uint16Array(limit), pairs = 0
+    for (let wt of people) weights[wt]++;                          // Make a frequency map of people's weights
+    for (let i = 0, j = limit; i <= j;) {                        // Initiate the sliding window
+        while (!weights[j] && j > i) j--;                     // Until you find a non-zero, slide j back
+        while (!weights[i] && i < j) i++;                     // Until you find a non-zero, slide i forward
+        let x = weights[i], y = weights[j];
+        if (i + j > limit) j--;                                   // If they're not a match, move on to the next j
+        else if (i === j) pairs += ~~(x / 2), j = 0;          // If they're the same, add half and force a break
+        else if (x <= y) weights[j] -= x, pairs += x, i++;    // Otherwise, take the lower amount and remove it
+        else weights[i] -= y, pairs += y, j--                //    from the higher amount before moving onward
+    }
+    return people.length - pairs                                  // Each pair means one less trip than the max
+};
+
+/**
  * // Definition for a Node.
  * function Node(val, children) {
  *    this.val = val === undefined ? 0 : val;
