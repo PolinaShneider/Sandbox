ccd90699d2605ae84081ce6b543af2595dba9796
Polina Shneider <beatrixlinkoln@gmail.com>
2021-01-20 02:45:54 +0300

    Tasks 19.01

--- interview.js
+++ interview.js
@@ -276,3 +276,26 @@ var summaryRanges = function (nums) {
 
     return result;
 };
+
+const throttle = (func, limit) => {
+    let inThrottle;
+    return function () {
+        const args = arguments;
+        const context = this;
+        if (!inThrottle) {
+            func.apply(context, args);
+            inThrottle = true;
+            setTimeout(() => inThrottle = false, limit)
+        }
+    }
+};
+
+const debounced = (func, delay) => {
+    let inDebounce;
+    return function () {
+        const context = this;
+        const args = arguments;
+        clearTimeout(inDebounce);
+        inDebounce = setTimeout(() => func.apply(context, args), delay)
+    }
+};
