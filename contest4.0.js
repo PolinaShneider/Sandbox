8e44b564f15e7866e95dea189fcc98b79946e093
Polina Shneider <beatrixlinkoln@gmail.com>
2021-01-25 01:23:20 +0300

    Tasks 24.01

--- contest4.0.js
+++ contest4.0.js
@@ -3341,3 +3341,42 @@ var closeStrings = function (word1, word2) {
     const str2 = freq2.filter(x => x).sort().join()
     return str1 === str2;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function (lists) {
+    let i = 0;
+
+    let result = null;
+
+    lists.forEach(v => {
+        result = mergeList(result, v);
+    });
+
+    return result;
+};
+
+var mergeList = (l1, l2) => {
+    if (l1 == null)
+        return l2;
+
+    if (l2 == null)
+        return l1;
+
+    if (l1.val < l2.val) {
+        l1.next = mergeList(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeList(l1, l2.next);
+        return l2;
+    }
+}
