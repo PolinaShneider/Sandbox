2093468506bef17d3e81b65d66d718e4a9472e47
Polina Shneider <beatrixlinkoln@gmail.com>
2021-02-01 00:40:34 +0300

    Tasks 31.01

--- contest4.0.js
+++ contest4.0.js
@@ -3544,3 +3544,22 @@ var minimumDeviation = function (nums) {
     return ans;
 };
 
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function (nums) {
+    let i, j;
+    // Find the first index from the right where the value is less than the next value. If this doesn't exist then i = -1.
+    for (i = nums.length - 2; nums[i] >= nums[i + 1] && i >= 0; i--) ;
+    if (i >= 0) {
+        // Find the first index from the right where the value is greater than the value found in previous step.
+        for (j = nums.length - 1; nums[j] <= nums[i]; j--) ;
+        // Swap the two values found.
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+    // Reverse everything after index i found above. Note if i = -1 then the whole array is reversed.
+    for (i++, j = nums.length - 1; i < j; i++, j--) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+};
