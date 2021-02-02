11694e2d0e4a4f6142e651a79a798f2bb4825c39
Polina Shneider <beatrixlinkoln@gmail.com>
2021-02-03 01:05:22 +0300

    Tasks 02.02

--- contest4.0.js
+++ contest4.0.js
@@ -3581,3 +3581,27 @@ var minDistance = function (height, width, tree, squirrel, nuts) {
 function dist(a, b) {
     return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
 }
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {TreeNode}
+ */
+var trimBST = function (root, low, high) {
+    if (root === null) return root;
+    if (root.val > high) return trimBST(root.left, low, high);
+    if (root.val < low) return trimBST(root.right, low, high);
+
+    root.left = trimBST(root.left, low, high);
+    root.right = trimBST(root.right, low, high);
+    return root;
+};
