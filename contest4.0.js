ef88ede12c449d8ec1bd93629882b56993e80c0d
Polina Shneider <beatrixlinkoln@gmail.com>
2021-02-12 00:01:55 +0300

    Tasks 11.02

--- contest4.0.js
+++ contest4.0.js
@@ -3855,3 +3855,67 @@ var copyRandomList = function (head) {
     linkRandomNode(head);
     return restoreOriginal(head);
 };
+
+/**
+ * // This is the Iterator's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * function Iterator() {
+ *    @ return {number}
+ *    this.next = function() { // return the next number of the iterator
+ *       ...
+ *    };
+ *
+ *    @return {boolean}
+ *    this.hasNext = function() { // return true if it still has numbers
+ *       ...
+ *    };
+ * };
+ */
+
+/**
+ * @param {Iterator} iterator
+ */
+var PeekingIterator = function (iterator) {
+    this.it = iterator;
+    this.peeked = false;
+    this.last = null;
+    this.has = this.it.hasNext();
+};
+
+/**
+ * @return {number}
+ */
+PeekingIterator.prototype.peek = function () {
+    if (!this.peeked) {
+        this.last = this.it.next();
+        this.peeked = true;
+    }
+    return this.last;
+};
+
+/**
+ * @return {number}
+ */
+PeekingIterator.prototype.next = function () {
+    if (!this.peeked) {
+        this.last = this.it.next();
+    }
+    this.has = this.it.hasNext();
+    this.peeked = false;
+    return this.last;
+};
+
+/**
+ * @return {boolean}
+ */
+PeekingIterator.prototype.hasNext = function () {
+    return this.has;
+};
+
+/**
+ * Your PeekingIterator object will be instantiated and called as such:
+ * var obj = new PeekingIterator(arr)
+ * var param_1 = obj.peek()
+ * var param_2 = obj.next()
+ * var param_3 = obj.hasNext()
+ */
