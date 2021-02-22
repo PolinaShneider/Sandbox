67eaf0b91e65b9fcd7b3050e946d9a18f971ae36
Polina Shneider <beatrixlinkoln@gmail.com>
2021-02-23 01:43:52 +0300

    Tasks 22.02

--- contest4.0.js
+++ contest4.0.js
@@ -4085,3 +4085,39 @@ var brokenCalc = function (X, Y) {
     }
     return count + (X - Y)
 };
+
+/**
+ * Definition for knows()
+ *
+ * @param {integer} person a
+ * @param {integer} person b
+ * @return {boolean} whether a knows b
+ * knows = function(a, b) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} knows()
+ * @return {function}
+ */
+var solution = function (knows) {
+    /**
+     * @param {integer} n Total people
+     * @return {integer} The celebrity
+     */
+    return function (n) {
+        let c = 0;
+        for (let i = 1; i < n; i++) {
+            if (knows(c, i)) {
+                c = i;
+            }
+        }
+        for (let i = 0; i < n; i++) {
+            if (i !== c && (knows(c, i) || !knows(i, c))) {
+                return -1;
+            }
+        }
+        return c;
+    };
+};
