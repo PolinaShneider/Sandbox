4471efacda8b93554597a3abdfdc8aa77134a90d
Polina Shneider <beatrixlinkoln@gmail.com>
2021-02-24 01:53:34 +0300

    Tasks 23.02

--- contest4.0.js
+++ contest4.0.js
@@ -4121,3 +4121,21 @@ var solution = function (knows) {
         return c;
     };
 };
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+    if (!matrix.length) return false;
+
+    let row = 0, col = matrix[0].length - 1;
+
+    while (row < matrix.length && col >= 0) {
+        if (matrix[row][col] === target) return true;
+        if (matrix[row][col] > target) col--;
+        else row++;
+    }
+    return false;
+};
