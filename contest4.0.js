37e2edb1f2c0736c974ee9f2160b15702cdc82fd
Polina Shneider <beatrixlinkoln@gmail.com>
2021-03-12 00:14:36 +0300

    Tasks 11.03

--- contest4.0.js
+++ contest4.0.js
@@ -4475,3 +4475,21 @@ var intToRoman = function (num) {
     return result;
 };
 
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+    const dp = Array(amount + 1).fill(amount + 1);
+    dp[0] = 0;
+
+    for (const coin of coins) {
+        for (let i = coin; i <= amount; i++) {
+            dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+        }
+    }
+
+    return dp[amount] > amount ? -1 : dp[amount];
+};
+
