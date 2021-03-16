bd8b201e58c94aade38feac855c313f4f1d8953a
Polina Shneider <beatrixlinkoln@gmail.com>
2021-03-17 01:18:13 +0300

    Tasks 16.03

--- contest4.0.js
+++ contest4.0.js
@@ -4626,3 +4626,36 @@ var str2tree = function (s) {
     return stack.length ? stack[0] : null;
 };
 
+/**
+ * @param {number[]} prices
+ * @param {number} fee
+ * @return {number}
+ */
+var maxProfit = function (prices, fee) {
+    let dp = {}
+
+    function iterate(buy, index) {
+        if (index === prices.length) {
+            return 0
+        }
+        if (dp[`${buy}-${index}`]) {
+            return dp[`${buy}-${index}`]
+        }
+        let profit = 0
+        if (buy) {
+            dp[`${buy}-${index}`] = Math.max(
+                iterate(false, index + 1) + prices[index],
+                iterate(buy, index + 1)
+            )
+        } else {
+            dp[`${buy}-${index}`] = Math.max(
+                iterate(buy, index + 1),
+                iterate(true, index + 1) - prices[index] - fee
+            )
+        }
+        return dp[`${buy}-${index}`]
+    }
+
+    return iterate(false, 0)
+};
+
