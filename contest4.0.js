033f8274b779fac270959aceb5121d955f30e104
Polina Shneider <beatrixlinkoln@gmail.com>
2021-03-19 01:21:05 +0300

    Tasks 18.03

--- contest4.0.js
+++ contest4.0.js
@@ -4686,3 +4686,16 @@ Solution.prototype.randPoint = function () {
  * var param_1 = obj.randPoint()
  */
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var wiggleMaxLength = function (nums) {
+    let up = 1, down = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) up = down + 1;
+        else if (nums[i] < nums[i - 1]) down = up + 1;
+    }
+    return Math.max(up, down)
+};
