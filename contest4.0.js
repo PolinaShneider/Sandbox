4c04901f23aea3552c456d084a12c101e0e9f28a
Polina Shneider <beatrixlinkoln@gmail.com>
2021-03-23 01:01:48 +0300

    Tasks 22.03

--- contest4.0.js
+++ contest4.0.js
@@ -4755,3 +4755,26 @@ var reorderedPowerOf2 = function (N) {
         if ((1 << i).toString().split("").sort().join("") === res) return true
     return false
 };
+
+/**
+ * @param {string[]} wordlist
+ * @param {string[]} queries
+ * @return {string[]}
+ */
+var spellchecker = function (wordlist, queries) {
+    const baseList = new Set(wordlist.reverse());
+    const loweredList = wordlist.reduce((map, word) => (map[normalize(word)] = word, map), {});
+    const replacedList = wordlist.reduce((map, word) => (map[repVowels(word)] = word, map), {});
+    return queries.map(word => (
+        baseList.has(word) && word ||
+        loweredList[normalize(word)] ||
+        replacedList[repVowels(word)] ||
+        ''
+    ));
+};
+var normalize = function (word) {
+    return word.toLowerCase()
+};
+var repVowels = function (word) {
+    return normalize(word).replace(/[eiou]/g, 'a')
+};
