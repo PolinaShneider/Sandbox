aae2752981941e59c02aa38578cd1909771f25a3
Polina Shneider <beatrixlinkoln@gmail.com>
2021-03-30 01:53:18 +0300

    Tasks 29.03

--- contest4.0.js
+++ contest4.0.js
@@ -5030,3 +5030,33 @@ var originalDigits = function (s) {
     return res;
 };
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number[]} voyage
+ * @return {number[]}
+ */
+var flipMatchVoyage = function (root, voyage) {
+    let i = 0
+    const result = []
+
+    const dfs = (node, v) => {
+        if (node == null) return true
+        if (node.val !== v[i++]) return false
+        if (node.left !== null && node.left.val !== v[i]) {
+            result.push(node.val)
+            return dfs(node.right, v) && dfs(node.left, v)
+        }
+        return dfs(node.left, v) && dfs(node.right, v)
+    }
+
+    return dfs(root, voyage) ? result : [-1]
+};
+
