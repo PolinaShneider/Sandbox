43821b7d45e61027fa227bedb131234b5159cb93
Polina Shneider <beatrixlinkoln@gmail.com>
2021-04-08 01:02:30 +0300

    Tasks 07.04

--- contest5.0.js
+++ contest5.0.js
@@ -5,3 +5,22 @@
 var minOperations = function(n) {
     return Math.floor(n * n / 4);
 };
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var halvesAreAlike = function(s) {
+    const isVowel = (char) => /[aeiuo]/i.test(char);
+    const countVowels = (str) => str.split('').reduce((total, item) => {
+        if (isVowel(item)) {
+            total++
+        }
+        
+        return total;
+    }, 0);
+    const first = s.slice(0, s.length / 2);
+    const second = s.slice(s.length / 2);
+    
+    return countVowels(first) === countVowels(second)
+};
