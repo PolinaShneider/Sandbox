19003bc18b29e2a4256b8bd28d340991c9eb07ab
Polina Shneider <beatrixlinkoln@gmail.com>
2021-04-10 01:34:50 +0300

    Tasks 09.04

--- contest5.0.js
+++ contest5.0.js
@@ -2,7 +2,7 @@
  * @param {number} n
  * @return {number}
  */
-var minOperations = function(n) {
+var minOperations = function (n) {
     return Math.floor(n * n / 4);
 };
 
@@ -10,18 +10,18 @@ var minOperations = function(n) {
  * @param {string} s
  * @return {boolean}
  */
-var halvesAreAlike = function(s) {
+var halvesAreAlike = function (s) {
     const isVowel = (char) => /[aeiuo]/i.test(char);
     const countVowels = (str) => str.split('').reduce((total, item) => {
         if (isVowel(item)) {
             total++
         }
-        
+
         return total;
     }, 0);
     const first = s.slice(0, s.length / 2);
     const second = s.slice(s.length / 2);
-    
+
     return countVowels(first) === countVowels(second)
 };
 
@@ -37,7 +37,7 @@ var halvesAreAlike = function(s) {
  * @param {TreeNode} p
  * @return {TreeNode}
  */
-var inorderSuccessor = function(root, p) {
+var inorderSuccessor = function (root, p) {
     const stack = [];
     let curr = root, isPassed = false;
     while (curr || stack.length) {
@@ -55,4 +55,47 @@ var inorderSuccessor = function(root, p) {
         curr = curr.right;
     }
     return null;
+};
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+const letterCombinations = function (digits) {
+    if (!digits.length) return [];
+
+    let obj = {
+        2: ['a', 'b', 'c'],
+        3: ['d', 'e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m', 'n', 'o'],
+        7: ['p', 'q', 'r', 's'],
+        8: ['t', 'u', 'v'],
+        9: ['w', 'x', 'y', 'z'],
+    };
+
+    let result = [];
+    const helperRecursion = str => {
+        if (str.length == 0) {
+            return;
+        } else {
+            let tempArr = [];
+            if (result.length === 0) {
+                tempArr = obj[str[0]];
+            } else {
+                const alphasArray = obj[str[0]];
+                const resultLength = result.length;
+                for (let i = 0; i < alphasArray.length; i++) {
+                    for (let j = 0; j < resultLength; j++) {
+                        tempArr.push(result[j] + alphasArray[i]);
+                    }
+                }
+            }
+            result = tempArr;
+            helperRecursion(str.substring(1));
+        }
+    }
+    helperRecursion(digits);
+    return result;
 };
