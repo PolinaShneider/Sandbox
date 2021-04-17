565133397dad14df270befe2988e6de0fc0a887e
Polina Shneider <beatrixlinkoln@gmail.com>
2021-04-18 00:52:14 +0300

    Tasks 17.04

--- contest5.0.js
+++ contest5.0.js
@@ -276,3 +276,30 @@ var removeDuplicates = function (s, k) {
     return rdStk.reduce((str, elm) => str + elm[0].repeat(elm[1]), '');
 };
 
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {number}
+ */
+var numSubmatrixSumTarget = function (matrix, target) {
+    let row = matrix.length, col = matrix[0].length;
+    for (let i = 1; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            matrix[i][j] += matrix[i - 1][j];
+        }
+    }
+    let ans = 0;
+    for (let i = 0; i < row; i++) {
+        for (let j = i; j < row; j++) {
+            let map = new Map(), sum = 0;
+            map.set(0, 1);
+            for (let k = 0; k < col; k++) {
+                sum += matrix[j][k] - (i != 0 ? matrix[i - 1][k] : 0);
+                ans += (map.get(sum - target) || 0);
+                map.set(sum, (map.get(sum) || 0) + 1);
+            }
+        }
+    }
+    return ans;
+};
+
