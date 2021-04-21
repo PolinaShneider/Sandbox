e3b975e18f2f4dcdb4c3b98c3e5a0879d28b26fe
Polina Shneider <beatrixlinkoln@gmail.com>
2021-04-22 00:47:15 +0300

    Tasks 21.04

--- contest5.0.js
+++ contest5.0.js
@@ -385,3 +385,16 @@ var preorder = function (root) {
     }
     return result;
 };
+
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal = function (triangle) {
+    for (let i = triangle.length - 2; ~i; i--) {
+        for (let j = 0; j < triangle.length; j++) {
+            triangle[i][j] += Math.min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+        }
+    }
+    return triangle[0][0];
+};
