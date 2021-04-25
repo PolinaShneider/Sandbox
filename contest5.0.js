7bea340a1d81ce2478fefccaf7033a622fce001a
Polina Shneider <beatrixlinkoln@gmail.com>
2021-04-26 01:11:10 +0300

    Tasks 25.04

--- contest5.0.js
+++ contest5.0.js
@@ -472,3 +472,26 @@ var criticalConnections = function(n, connections) {
   dfs(-1, 0, 0);
   return results;
 };
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+     // Firstly Transpose The Matrix
+   for (let i = 0 ; i < matrix.length;i++){
+       for (let j = i ; j < matrix.length;j++){
+           var Temp = matrix[j][i]
+           matrix[j][i] = matrix[i][j]
+           matrix[i][j] = Temp
+       }
+   }
+   // Secondly Make Reflected Image Of Matrix
+   for (let i = 0 ; i < matrix.length;i++){
+       for (let j = 0 ; j < matrix.length / 2 ;j++){
+           var Temp = matrix[i][j]
+           matrix[i][j] = matrix[i][matrix.length-j-1]
+           matrix[i][matrix.length - j - 1] = Temp
+       }
+   }
+};
