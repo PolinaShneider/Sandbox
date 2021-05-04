41205d616b7bce683cdd06fe9f6834c7f518e9a4
Polina Shneider <beatrixlinkoln@gmail.com>
2021-05-05 01:25:05 +0300

    Tasks 04.05

--- contest5.0.js
+++ contest5.0.js
@@ -696,3 +696,23 @@ var runningSum = function (nums) {
         return total;
     }, []);
 };
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var checkPossibility = function(nums) {
+  let count = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] < nums[i - 1]) {
+      if (nums[i] < nums[i - 2]) {
+        nums[i] = nums[i - 1];
+      }
+      count++;
+    }
+    if (count > 1) {
+      return false;
+    }
+  }
+  return true;
+};
