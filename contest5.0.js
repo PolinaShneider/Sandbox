e71e9f4ce817b60ce68878fc49c5d169efded8b5
Polina Shneider <beatrixlinkoln@gmail.com>
2021-05-10 01:22:42 +0300

    Tasks 09.05

--- contest5.0.js
+++ contest5.0.js
@@ -843,3 +843,22 @@ var superpalindromesInRange = function (left, right) {
     }
     return ans
 };
+
+/**
+ * @param {number[]} target
+ * @return {boolean}
+ */
+var isPossible = function (T) {
+    if (T.length === 1 && T[0] !== 1) return false
+    let sum = T.reduce((a, b) => a + b)
+    T.sort((a, b) => b - a)
+
+    while (sum !== T.length) {
+        let m = T[0] - (sum - T[0]) * (Math.trunc(T[0] / (sum - T[0]) - 1) || 1);
+        [sum, T[0]] = [sum - T[0] + m, m]
+        if (T[0] < 1) return false
+        for (let i = 0; T[i] < T[i + 1]; i++) [T[i], T[i + 1]] = [T[i + 1], T[i]]
+    }
+
+    return true
+};
