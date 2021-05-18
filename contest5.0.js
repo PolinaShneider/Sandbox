fc5a6f66c46b3f59e065ec2244d54a6e5c17f781
Polina Shneider <beatrixlinkoln@gmail.com>
2021-05-19 00:40:38 +0300

    Tasks 18.05

--- contest5.0.js
+++ contest5.0.js
@@ -1270,3 +1270,22 @@ var longestStrChain = function (words) {
 
     return Math.max(...Object.values(memory));
 };
+
+/**
+ * @param {string[]} paths
+ * @return {string[][]}
+ */
+const findDuplicate = (paths) => {
+    let m = new Map();
+    for (const p of paths) {
+        let pa = p.split(" ");
+        for (let i = 1; i < pa.length; i++) {
+            let start = pa[i].indexOf('(');
+            let fileName = pa[i].slice(0, start);
+            let content = pa[i].slice(start + 1, -1);
+            if (!m.has(content)) m.set(content, []);
+            m.get(content).push(pa[0] + '/' + fileName);
+        }
+    }
+    return Array.from(m.values()).filter(x => x.length > 1);
+};
