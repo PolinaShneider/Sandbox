45a7aa13d6f4e5d0c26ac8e29dcb275b4844ee43
Polina Shneider <beatrixlinkoln@gmail.com>
2021-05-24 01:52:15 +0300

    Tasks 23.05

--- contest5.0.js
+++ contest5.0.js
@@ -1328,7 +1328,7 @@ var TicTacToe = function (n) {
  * @return {number}
  */
 TicTacToe.prototype.move = function (row, col, player) {
-    let update = player == 1 ? 1 : -1
+    let update = player === 1 ? 1 : -1;
     this.row[row] += update
     this.col[col] += update
     if (row == col) this.diagonal[0] += update;
@@ -1344,3 +1344,49 @@ TicTacToe.prototype.move = function (row, col, player) {
  * var obj = new TicTacToe(n)
  * var param_1 = obj.move(row,col,player)
  */
+
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+const shortestSuperstring = (A) => {
+    function additionalStr(a, b) { // a + b additional Str
+        for (let i = 0; i < a.length; i++) {
+            if (b.startsWith(a.slice(i))) {
+                return b.slice(a.length - i);
+            }
+        }
+        return b;
+    }
+
+    // dp[i][j] -> min str building j state ending word i
+    let dp = new Array(A.length).fill()
+        .map(() => new Array(1 << A.length).fill(A.join('')));
+
+    for (let s = 1; s < (1 << A.length); s++) {
+        for (let j = 0; j < A.length; j++) {
+            if (!(s & (1 << j))) continue;
+            if (s === (1 << j)) dp[j][s] = A[j];
+            let prevS = s ^ (1 << j);
+            for (let k = 0; k < A.length; k++) {
+                if (prevS & (1 << k)) {
+                    let curStr = dp[k][prevS];
+                    let tempStr = curStr + additionalStr(curStr, A[j]);
+                    if (tempStr.length < dp[j][s].length) {
+                        dp[j][s] = tempStr;
+                    }
+                }
+            }
+        }
+    }
+    let min = Number.MAX_VALUE;
+    let minStr = null;
+    for (let i = 0; i < A.length; i++) {
+        let cur = dp[i][(1 << (A.length)) - 1];
+        if (cur.length < min) {
+            min = cur.length;
+            minStr = cur;
+        }
+    }
+    return minStr;
+}
