906c58e1ba8a1e1bd1b24fc013489eed1eb08752
Polina Shneider <beatrixlinkoln@gmail.com>
2021-05-26 00:55:53 +0300

    Tasks 25.05

--- contest5.0.js
+++ contest5.0.js
@@ -1398,3 +1398,26 @@ const shortestSuperstring = (A) => {
 var toLowerCase = function (str) {
     return str.toLowerCase()
 };
+
+// JS
+const evaluate = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => Math.trunc(a / b),
+};
+
+var evalRPN = function (tokens) {
+    const stack = [];
+
+    while (tokens.length) {
+        const t = tokens.shift();
+        if (t in evaluate) {
+            const b = stack.pop();
+            const a = stack.pop();
+            stack.push(evaluate[t](a, b));
+        } else stack.push(Math.trunc(Number(t)));
+    }
+
+    return stack[0];
+};
