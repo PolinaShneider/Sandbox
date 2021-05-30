322a34d3bd35f759e15c7a88d35702f0db2e577d
Polina Shneider <beatrixlinkoln@gmail.com>
2021-05-31 02:08:30 +0300

    Tasks 30.05

--- contest5.0.js
+++ contest5.0.js
@@ -1510,3 +1510,18 @@ const connectSticks = sticks => {
     }
     return res
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumGap = function (nums) {
+    if (nums.length < 2) return 0;
+    nums = nums.sort((a, b) => a - b);
+    let max = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i + 1] - nums[i] > max)
+            max = nums[i + 1] - nums[i];
+    }
+    return max;
+};
