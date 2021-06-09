97e687796a6f23d622bafed872c079c48e8ed97f
Polina Shneider <beatrixlinkoln@gmail.com>
2021-06-10 02:58:16 +0300

    Tasks 09.06

--- contest5.0.js
+++ contest5.0.js
@@ -1807,3 +1807,19 @@ const buildTree = (preorder, inorder) => {
     for (let i = 0; i < inorder.length; i++) map.set(inorder[i], i);
     return helper(preorder, map, 0, preorder.length, 0, inorder.length);
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxResult = function (nums, k) {
+    let n = nums.length, deq = [n - 1]
+    for (let i = n - 2; ~i; i--) {
+        if (deq[0] - i > k) deq.shift()
+        nums[i] += nums[deq[0]]
+        while (deq.length && nums[deq[deq.length - 1]] <= nums[i]) deq.pop()
+        deq.push(i)
+    }
+    return nums[0]
+};
