6c2295d3a59608cf0aa35a85be87f78db540da6b
Polina Shneider <beatrixlinkoln@gmail.com>
2021-06-11 00:25:57 +0300

    Tasks 10.06

--- contest5.0.js
+++ contest5.0.js
@@ -1823,3 +1823,29 @@ var maxResult = function (nums, k) {
     }
     return nums[0]
 };
+
+var MyCalendar = function () {
+    this.cal = []
+};
+
+MyCalendar.prototype.book = function (start, end) {
+    let l = 0, r = this.cal.length - 1
+    while (l <= r) {
+        const mid = Math.floor((r + l) / 2)
+        const [s, e] = this.cal[mid]
+        if (s < end && start < e) return false
+        if (start >= e) {
+            l = mid + 1
+        } else {
+            r = mid - 1
+        }
+    }
+    this.cal.splice(l, 0, [start, end])
+    return true
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * var obj = new MyCalendar()
+ * var param_1 = obj.book(start,end)
+ */
