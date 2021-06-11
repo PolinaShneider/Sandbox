5426d686c0fd6dfe52d48c590bb569b905de6b50
Polina Shneider <beatrixlinkoln@gmail.com>
2021-06-12 02:09:39 +0300

    Tasks 11.06

--- contest5.0.js
+++ contest5.0.js
@@ -1849,3 +1849,15 @@ MyCalendar.prototype.book = function (start, end) {
  * var obj = new MyCalendar()
  * var param_1 = obj.book(start,end)
  */
+
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var stoneGameVII = function (s) {
+    let len = s.length, dp = new Array(len).fill().map(_ => new Array(len).fill(0))
+    for (let i = len - 2; ~i; i--)
+        for (let j = i + 1, sum = s[i] + s[j]; j < len; sum += s[++j])
+            dp[i][j] = Math.max(sum - s[i] - dp[i + 1][j], sum - s[j] - dp[i][j - 1])
+    return dp[0][len - 1]
+};
