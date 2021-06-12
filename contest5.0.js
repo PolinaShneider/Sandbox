20f275b02efbd0e276158ad2308c9a909f375eeb
Polina Shneider <beatrixlinkoln@gmail.com>
2021-06-13 02:01:45 +0300

    Tasks 12.06

--- contest5.0.js
+++ contest5.0.js
@@ -1861,3 +1861,32 @@ var stoneGameVII = function (s) {
             dp[i][j] = Math.max(sum - s[i] - dp[i + 1][j], sum - s[j] - dp[i][j - 1])
     return dp[0][len - 1]
 };
+
+/**
+ * @param {number} target
+ * @param {number} startFuel
+ * @param {number[][]} stations
+ * @return {number}
+ */
+var minRefuelStops = function (target, startFuel, stations) {
+    let curr = startFuel;
+    let len = stations.length;
+    stations.sort(function (x, y) {
+        return x[0] - y[0]
+    });
+    let pq = new Array();
+    let i = 0, count = 0;
+    while (curr < target) {
+        count++;
+        while (i < len && stations[i][0] <= curr) {
+            pq.push(stations[i][1]);
+            i++;
+        }
+        if (!pq.length)
+            break;
+        let max = Math.max(...pq);
+        curr += max;
+        pq.splice(pq.indexOf(max), 1);
+    }
+    return curr >= target ? count : -1;
+};
