7ea98f6084fb331eb9368302ec29665eb4d39254
Polina Shneider <beatrixlinkoln@gmail.com>
2021-07-01 00:36:59 +0300

    Tasks 30.06

--- contest5.0.js
+++ contest5.0.js
@@ -2363,3 +2363,25 @@ var isArmstrong = function (n) {
 
     return tot === n;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+    if (!root || root === p || root === q) return root;
+    var left = lowestCommonAncestor(root.left, p, q);
+    var right = lowestCommonAncestor(root.right, p, q);
+    if (left && right) return root;
+    if (!left) return right;
+    return left;
+};
