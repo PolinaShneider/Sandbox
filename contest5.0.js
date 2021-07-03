d4b9463ed0240ad888019560b35bbf79d23a4f34
Polina Shneider <beatrixlinkoln@gmail.com>
2021-07-04 00:53:17 +0300

    Tasks 03.07

--- contest5.0.js
+++ contest5.0.js
@@ -2426,3 +2426,32 @@ var findClosestElements = function (arr, k, x) {
     closestChain.sort((a, b) => a.subs - b.subs);
     return closestChain.slice(0, k).map(a => a.val).sort((a, b) => a - b);
 };
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+var maxSumSubmatrix = function (m, k) {
+    let max = -Infinity;
+    const rows = m.length, cols = m[0].length;
+    const sums = Array(rows);
+    for (let c1 = 0; c1 < cols; c1++) {
+        sums.fill(0);
+        for (let c2 = c1; c2 < cols; c2++) {
+            for (let row = 0; row < rows; row++) {
+                sums[row] += m[row][c2];
+            }
+            for (let i = 0; i < rows; i++) {
+                let sum = 0;
+                for (let j = i; j < rows; j++) {
+                    sum += sums[j];
+                    if (sum > max && sum <= k) {
+                        max = sum;
+                    }
+                }
+            }
+        }
+    }
+    return max;
+};
