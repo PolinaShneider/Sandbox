db241899059f1f266eda3b23af23a57316a3b826
Polina Shneider <beatrixlinkoln@gmail.com>
2021-07-05 00:05:44 +0300

    Tasks 04.07

--- contest5.0.js
+++ contest5.0.js
@@ -2455,3 +2455,26 @@ var maxSumSubmatrix = function (m, k) {
     }
     return max;
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countVowelPermutation = function (n) {
+    const MOD = 1000000007;
+    const dp = Array(n + 1).fill(null).map(() => Array(5).fill(0));
+    dp[1][0] = 1;    // a
+    dp[1][1] = 1;    // e
+    dp[1][2] = 1;    // i
+    dp[1][3] = 1;    // o
+    dp[1][4] = 1;    // u
+
+    for (let i = 2; i <= n; ++i) {
+        dp[i][0] = dp[i - 1][1];                                                       // ae
+        dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;                                // ea, ei
+        dp[i][2] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][3] + dp[i - 1][4]) % MOD;  // ia, ie, io, iu
+        dp[i][3] = (dp[i - 1][2] + dp[i - 1][4]) % MOD;                                // oi, ou
+        dp[i][4] = dp[i - 1][0];                                                       // ua
+    }
+    return (dp[n][0] + dp[n][1] + dp[n][2] + dp[n][3] + dp[n][4]) % MOD;
+};
