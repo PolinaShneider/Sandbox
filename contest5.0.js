0795548044fd6ade5b1034ee53f13a4a4c16ea95
Polina Shneider <beatrixlinkoln@gmail.com>
2021-07-14 02:12:58 +0300

    Tasks 13.07

--- contest5.0.js
+++ contest5.0.js
@@ -2666,3 +2666,19 @@ const isIsomorphic = function (s, t) {
     }
     return true
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+    let [max, idx] = [-Infinity, -1]
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > max) {
+            [max, idx] = [nums[i], i]
+        }
+    }
+
+    return idx;
+};
